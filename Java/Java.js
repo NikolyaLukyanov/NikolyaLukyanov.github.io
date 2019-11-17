cf7797b7a47e4fa6006c794fef03c3bdd3f7ebb4
NikolyaLukyanov <54885268+NikolyaLukyanov@users.noreply.github.com>
2019-11-17 16:43:13 +0300

    site

--- Java/Java.js
+++ Java/Java.js
@@ -1,7 +1,13 @@
+
 var speed = {
 	'trigger': '#speed-compare',
 	'target':'.modal_content .speedometer'
 };
+$(window).scroll(function(){
+    if ( $(this).scrollTop() > el.offset().top - 200 ) {
+        el.addClass('play');
+    }
+    })
 $(function() {
 	$(speed.trigger).on('click', function() {
 		setTimeout( function() { 
