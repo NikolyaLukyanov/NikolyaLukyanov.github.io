c8b40a2a73fde703fa2eaf89c60a4278fd08c2a8
NikolyaLukyanov <54885268+NikolyaLukyanov@users.noreply.github.com>
2020-02-24 20:32:33 +0300

    test

--- Курсовая работа/Test.py
+++ Курсовая работа/Test.py
@@ -1,16 +1,45 @@
 import requests
+import time
+import csv
 
 
-tocken = 'cd6ad61ccd6ad61ccd6ad61c63cd056944ccd6acd6ad61c9323af835d1afa79027bd29e'
-version = 5.103
-domain='bmstu1830'
+def take_1000_posts():
+    tocken = 'cd6ad61ccd6ad61ccd6ad61c63cd056944ccd6acd6ad61c9323af835d1afa79027bd29e'
+    version = 5.103
+    domain = 'bmstu1830'
+    offset = 0
+    count = 100
+    all_posts = []
+
+    while offset < 1000:
+        response = requests.get('https://api.vk.com/method/wall.get',
+                                params={
+                                    'access_token': tocken,
+                                    'v': version,
+                                    'domain': domain
+                                })
+        data = response.json()['response']['items']
+        offset += 100
+        all_posts.extend(data)
+        time.sleep(0.5)
+    return all_posts
 
 
-response= requests.get('https://api.vk.com/method/wall.get',
-                       params={
-                           'access_token': tocken,
-                           'v': version,
-                           'domain':domain
-                       })
-data = response.json()
-print(1)
+def file_writer(data):
+    with open('vk.csv', 'w') as file:
+        a_pen = csv.writer(file)
+        a_pen.writerow(('likes', 'body', 'url'))
+        for post in data:
+            try:
+                if post['attachments'][0]['type']:
+                    img_url = post['attachment'][0]['photo']['sizes'][-1]['url']
+                else:
+                    img_url = 'pass'
+            except:
+                pass
+            a_pen.writerow((post['likes']['count'], post['text'], img_url))
+
+
+all_posts = take_1000_posts()
+file_writer(all_posts)
+
