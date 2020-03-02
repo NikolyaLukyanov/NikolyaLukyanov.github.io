cdd853c089816098093a777da4c912c54ea66802
NikolyaLukyanov <54885268+NikolyaLukyanov@users.noreply.github.com>
2020-03-02 19:17:45 +0300

    Update ttttt.py

--- Курсовая работа/ttttt.py
+++ Курсовая работа/ttttt.py
@@ -1,40 +1,14 @@
-import requests
-import time
-import csv
-
-
-def take_1000_posts():
-    tocken = 'cd6ad61ccd6ad61ccd6ad61c63cd056944ccd6acd6ad61c9323af835d1afa79027bd29e'
-    version = 5.103
-    domain = 'bmstu1830'
-    offset = 0
-    count = 100
-    all_posts = []
-
-    while offset < 1000:
-        response = requests.get('https://api.vk.com/method/wall.get',
-                                params={
-                                    'access_token': tocken,
-                                    'v': version,
-                                    'domain': domain
-                                })
-        data = response.json()['response']['items']
-        offset += 100
-        all_posts.extend(data)
-        time.sleep(0.5)
-    return all_posts
-
-
 import mysql.connector
 from mysql.connector import Error
 
 def connect():
     """ Connect to MySQL database """
+    global conn
     try:
         conn = mysql.connector.connect(host='localhost',
                                        database='vk',
                                        user='root',
-                                       password='Basketboll2002')
+                                       password='')
         if conn.is_connected():
             print('Connected to MySQL database')
 
@@ -46,4 +20,3 @@ def connect():
 
 if __name__ == '__main__':
     connect()
-print(1)
